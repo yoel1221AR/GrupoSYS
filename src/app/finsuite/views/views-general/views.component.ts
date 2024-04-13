73443a4b0d02e7d86d8cf13b7bdceceb76bd55a6
Yoel Mandelli <yoel.mandelli@techforb.com>
2024-04-13 19:25:28 -0300

    fix

--- src/app/finsuite/views/views-general/views.component.ts
+++ src/app/finsuite/views/views-general/views.component.ts
@@ -1,5 +1,5 @@
 import { ViewportScroller } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-views',
@@ -9,19 +9,44 @@ import { Component, HostListener } from '@angular/core';
 export class ViewsComponent {
   activeLink: string = 'home';
   isSticky: boolean = false;
-  constructor(private viewportScroller: ViewportScroller) {}
+
+  navbarHeight: number = 80; // Altura del navbar, ajusta según tu diseño
+  offset: number = 200; // Offset adicional, ajusta según sea necesario
+
+  constructor(
+    private viewportScroller: ViewportScroller,
+    private el: ElementRef
+  ) {}
 
   scrollToSection(id: string): void {
-    this.viewportScroller.scrollToAnchor(id);
+    const element = document.getElementById(id);
+    if (element !== null) {
+      const y =
+        element.getBoundingClientRect().top +
+        window.pageYOffset -
+        this.navbarHeight -
+        this.offset;
+      window.scrollTo({ top: y, behavior: 'smooth' });
+    }
   }
+
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const scrollPosition =
-      window.pageYOffset ||
-      document.documentElement.scrollTop ||
-      document.body.scrollTop ||
-      0;
-    this.isSticky = scrollPosition > 100; // Cambia 100 por la distancia desde la parte superior en la que quieres que el navbar se fije
+      window.pageYOffset || document.documentElement.scrollTop || 0;
+    const sections: HTMLElement[] = Array.from(
+      this.el.nativeElement.querySelectorAll('section')
+    );
+    let currentSection = 'home';
+    sections.forEach((section: HTMLElement) => {
+      const sectionTop = section.offsetTop - this.navbarHeight - this.offset;
+      const sectionBottom = sectionTop + section.offsetHeight;
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        currentSection = section.id;
+      }
+    });
+    this.activeLink = currentSection;
+    this.isSticky = scrollPosition > 100;
   }
 
   activateLink(link: string): void {
